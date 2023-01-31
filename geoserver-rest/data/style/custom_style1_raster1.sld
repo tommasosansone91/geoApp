0ffaebd6d2f3e496acabe3909f58202247e8cfc2
tommasosansone91 <tommaso.sansone91@virgilio.it>
2023-02-01 00:47:37 +0100

    finished lesson 23 geoserver-rest crash course

---
+++ geoserver-rest/data/style/custom_style1_raster1.sld
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:sld="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:gml="http://www.opengis.net/gml" version="1.0.0">
+  <UserLayer>
+    <sld:LayerFeatureConstraints>
+      <sld:FeatureTypeConstraint/>
+    </sld:LayerFeatureConstraints>
+    <sld:UserStyle>
+      <sld:Name>raster1</sld:Name>
+      <sld:FeatureTypeStyle>
+        <sld:Rule>
+          <sld:RasterSymbolizer>
+            <sld:ChannelSelection>
+              <sld:GrayChannel>
+                <sld:SourceChannelName>1</sld:SourceChannelName>
+              </sld:GrayChannel>
+            </sld:ChannelSelection>
+            <sld:ColorMap type="values">
+              <sld:ColorMapEntry color="#d95d54" label="customclass1" quantity="1"/>
+              <sld:ColorMapEntry color="#88e28e" label="customclass2" quantity="2"/>
+              <sld:ColorMapEntry color="#6d8ce3" label="customclass3" quantity="3"/>
+            </sld:ColorMap>
+          </sld:RasterSymbolizer>
+        </sld:Rule>
+      </sld:FeatureTypeStyle>
+    </sld:UserStyle>
+  </UserLayer>
+</StyledLayerDescriptor>
--- geoserver-rest/geoserver-rest.py
+++ geoserver-rest/geoserver-rest.py
@@ -5,22 +5,31 @@
 
 # folloiwng https://pypi.org/project/geoserver-rest/
 
+#  activate the venv
+# source venv/bin/activate
+
+# activate geoserver
+# cd /usr/share/geoserver/bin/ && sh startup.sh 
+
 # launch this script by  
 # python geoserver-rest.py
 
 # Import the library
 from geo.Geoserver import Geoserver
 
-# Initialize the library
+
+
+# Initialize the library : istanziare un geoserver
 geo = Geoserver('http://127.0.0.1:8080/geoserver', username='admin', password='geoserver')
 
 # devo tenere aperta una finestra web su geoserver.
 # geoserver va startato
 # devi loggarti con user e pw
 
+# start anew workspace each time - every object attached to it would be deleted in cascade ?
+geo.delete_workspace(workspace='demo')
 
-
-# geo.create_workspace('demo')
+geo.create_workspace('demo')
 # crea workspace accessibile dall ainterfaccia da menu di sx
 # workspaces > demo
 
@@ -46,6 +55,35 @@ geo.create_coveragestore( layer_name='raster1', path=r'data/raster/raster1.tif',
 # per importare ed esportare i metadata feature json data usiamo il manager di postgis che apor dalla barra di comando
 # postgis shapefile import export manager
 
-geo.create_featurestore(store_name='geo_data', workspace='demo', db='postgres', host='localhost', pg_user='postgres',
-                        pg_password='admin')
-geo.publish_featurestore(workspace='demo', store_name='geo_data', pg_table='geodata_table_name')
+# we want to publish the layer, so we use createfeaturestore.
+# prima lo creo, poi lo pubblico
+
+geo.create_featurestore(store_name='postgis', workspace='demo', db='postgres', host='localhost', pg_user='postgres', pg_password='password')
+
+geo.publish_featurestore(workspace='demo', store_name='postgis', pg_table='jamoat-db')
+
+# now i want to apply a style to the uploaded raster file.
+# styles are file which are part of the group of a shapefile.
+# this upload the stile to the db, in the workspace, i can see it in geoserver in style tab
+# http://localhost:8080/geoserver/web/wicket/bookmarkable/org.geoserver.wms.web.data.StylePage?5&filter=false
+
+# carico raster1.tif dai data in qgis
+
+geo.upload_style(path='data/style/custom_style1_raster1.sld', workspace='demo')
+
+geo.upload_style(path='data/style/raster1.sld', workspace='demo')
+
+#  now i publish the style
+geo.publish_style(layer_name='raster1', style_name='raster1', workspace='demo')
+
+# now i create coverage style
+# the name i give to the style is raster-new, i will see it in thestyle tab of geoserver UI
+geo.create_coveragestyle(raster_path='data/raster/raster1.tif', style_name='raster-new', workspace='demo', color_ramp='hsv')
+
+
+#  now i publish the style
+geo.publish_style(layer_name='raster-new', style_name='raster1', workspace='demo')
+
+
+geo.create_outline_featurestyle('polygon-style', workspace='demo')
+geo.publish_style(layer_name='jamoat-db', style_name='polygon-style', workspace='demo')
