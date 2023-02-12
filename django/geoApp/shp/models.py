ade27e21524be1a78b214a78c78b20e65ca75569
tommasosansone91 <tommaso.sansone91@virgilio.it>
2023-02-12 23:29:22 +0100

    ended lesson 31. built the model shp

--- django/geoApp/shp/models.py
+++ django/geoApp/shp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Shp(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000, blank=True)
+    file = models.FileField(upload_to='%Y/%m/%d')
+    uploaded_date = models.DateField(default=datetime.date.today, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ geoproject/frontend_readme.txt
@@ -0,0 +1,3 @@
+when I am developing the forntend part,
+ I move its component into the django project.
+I am leaving here the source components.
