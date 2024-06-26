f1c4eba2ece7cfc2eef026d98857f3438eb66a46
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-06-27 00:54:15 +0200

    improve path to save files

--- django/geoApp/shp/configs.py
+++ django/geoApp/shp/configs.py
@@ -4,5 +4,12 @@ from geoApp.settings import UPLOADED_FILES_DIR
 
 UPLOADED_SHP_FILES_DIR = 'shp'
 
-uploaded_shp_files_relpath = os.path.join(UPLOADED_FILES_DIR, UPLOADED_SHP_FILES_DIR, '%Y/%m/%d')
+import datetime
+
+def generate_current_timestamp():
+    return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+# uploaded_shp_files_relpath = os.path.join(UPLOADED_FILES_DIR, UPLOADED_SHP_FILES_DIR, '%Y/%m/%d')
+# uploaded_shp_files_relpath = os.path.join(UPLOADED_FILES_DIR, UPLOADED_SHP_FILES_DIR, generate_current_timestamp())
+uploaded_shp_files_relpath = os.path.join(UPLOADED_FILES_DIR, UPLOADED_SHP_FILES_DIR)
 
