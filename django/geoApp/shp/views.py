4f4abd4853e1f843ce39a69dbd3a29460baf3f33
tommasosansone91 <tommaso.sansone91@virgilio.it>
2024-08-01 22:27:50 +0200

    add model elements call to geoapp views.py

--- django/geoApp/shp/views.py
+++ django/geoApp/shp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from shp.models import Shp
+from tiff.models import Tiff
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    shp = Shp.objects.all()
+    tiff = Tiff.objects.all()
+
+    context = {
+        'shp': shp,
+        'tiff': tiff
+    }
+    
+    return render(request, 'index.html', context)
